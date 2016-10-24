2f3ded36d8cca028549ad1ad2b997cfb84509140
JustinTharp <justintharp94@gmail.com>
2016-10-24 17:19:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/justin/androidactionbar/text_fragment.java
@@ -0,0 +1,24 @@
+package com.example.justin.androidactionbar;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class text_fragment extends Fragment {
+    TextView text;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_text_fragment, container, false);
+        text = (TextView) view.findViewById(R.id.txt);
+        String menu = getArguments().getString("Menu");
+        text.setText(menu);
+        return view;
+    }
+}
